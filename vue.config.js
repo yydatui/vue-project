96bf57115f93212baffc3e1c2ba2dcbf565f5508
leandrowhy <1229911200@qq.com>
2020-07-10 09:00:58 +0800

    first commit

---
+++ vue.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+//拼接路径
+function resolve(dir) {
+  return path.join(__dirname, dir)
+}
+module.exports = {
+  chainWebpack: (config) => {
+    config.resolve.alias
+      .set('@', resolve('src'))
+      .set('@components', resolve('src/components'))
+      .set('@assets', resolve('src/assets'))
+      .set('@views', resolve('src/views'))
+      .set('@request', resolve('src/request'))
+      .set('@common', resolve('src/components/common'))
+      .set('@content', resolve('src/components/content'))
+      .set('@utils', resolve('src/utils'))
+  },
+}
