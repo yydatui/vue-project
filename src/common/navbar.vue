8312d1564b4ddc9c7aaf62c467067fcbb4b77e20
leandrowhy <1229911200@qq.com>
2020-07-13 08:38:55 +0800

    init

---
+++ src/common/navbar.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="nav">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: '',
+  data() {
+    return {
+
+    }
+  },
+ }
+</script>
+
+<style scoped>
+.nav{
+  width: 1000px;
+}
+</style>
