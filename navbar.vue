6008849bff4b1ca4c9aec68c7039937daf8d4957
if-false1 <118348428@qq.com>
2020-07-13 09:22:03 +0800

    init

---
+++ navbar.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="nav">
+    <div class="logo"></div>
+    <div class="top-input">
+      <el-input placeholder="请输入内容" v-model="input" clearable></el-input>
+    </div>
+    <div class="top-right"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "navbar",
+  data() {
+    return {
+      input: ""
+    };
+  }
+};
+</script>
+
+<style scoped>
+.nav {
+  width: 1000px;
+  margin: 0 auto;
+}
+</style>
