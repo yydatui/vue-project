786d0bd40f8c49b52da6fa4d3b9bab349e19645a
if-false1 <118348428@qq.com>
2020-07-14 16:27:19 +0800

    commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,34 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+<<<<<<< HEAD
+=======
 import Home from '../views/Home.vue'
 import ShouCang from '../views/shouCang.vue'
 
+>>>>>>> commit
 Vue.use(VueRouter)
 
-  const routes = [
-  {
+
+import Index from '@views/index'
+import Login from '@views/login'
+
+const routes = [{
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'Index',
+    component: Index
   },
   {
+<<<<<<< HEAD
+    path: '/index',
+    name: 'Index',
+    component: Index
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login
+  }
+=======
     path: '/about',
     name: 'About',
     // route level code-splitting
@@ -23,12 +40,24 @@ Vue.use(VueRouter)
 		name: 'ShouCang',
 		component: ShouCang
 	}
+>>>>>>> commit
 ]
 
+// const router = new VueRouter({
+//   mode: 'history',
+//   base: process.env.BASE_URL,
+//   routes
+// })
 const router = new VueRouter({
+  routes,
   mode: 'history',
-  base: process.env.BASE_URL,
-  routes
+  scrollBehavior(to, from, savedPosition) {
+    if (to.hash) {
+      return {
+        selector: to.hash
+      }
+    }
+  }
 })
 
 export default router
